c80cf8da1e0d65e3166c1cb17eaac534dd0cca45
Bibidh Shrestha <sth_newb@live.com>
2020-07-10 10:23:38 +1000

    Updates

---
+++ precache-manifest.31cf4998f42547579318bede4b6c5347.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6a105d4db934c531099588d5285b7d",
+    "url": "/github.io/Bibidh.git/index.html"
+  },
+  {
+    "revision": "936672c9118eadcb5ea1",
+    "url": "/github.io/Bibidh.git/static/css/main.2709af23.chunk.css"
+  },
+  {
+    "revision": "d6b42633be048d1e96fc",
+    "url": "/github.io/Bibidh.git/static/js/2.a2646922.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/github.io/Bibidh.git/static/js/2.a2646922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936672c9118eadcb5ea1",
+    "url": "/github.io/Bibidh.git/static/js/main.74ac6f84.chunk.js"
+  },
+  {
+    "revision": "eae6796b0f591f2f0f0a",
+    "url": "/github.io/Bibidh.git/static/js/runtime-main.95f52e1e.js"
+  },
+  {
+    "revision": "89d07878c7c607bfc07bc79eb33a2e6b",
+    "url": "/github.io/Bibidh.git/static/media/BibidhShrestha.89d07878.docx"
+  },
+  {
+    "revision": "593c88261f3a7c1dc70db06bfb7f85dc",
+    "url": "/github.io/Bibidh.git/static/media/avatar.593c8826.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/Bibidh.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
